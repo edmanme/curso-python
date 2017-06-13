2becb53d17aa50d9eec6c01666549360b642fdeb
edmanmena <edmanmena2014@gmail.com>
2017-06-13 17:30:10 -0600

    prueba2 y tarea

--- cadenas.py
+++ cadenas.py
@@ -1,10 +1,9 @@
-dato=(input ("introduce texto: "))
+dato= ("esto es una prueba")
 text = dato
-conteo=0
+conteo = 0
 for letra in text:
-	dato2=(input ("introduce letra a contar: "))
-	if letra == dato2:
-		conteo+=1
-		print("el dato ",dato," tiene ", conteo," letras ",dato2)
-		print(text.upper())
-		print("la longitud de la cadena es de ", len(dato))
+	if letra == "e":
+		conteo += 1
+print("el ",dato," tiene ", conteo," letras e")
+print(text.upper())
+print("la longitud de la cadena es de ", len(dato))
---
+++ prueba2.py
@@ -0,0 +1,9 @@
+import random
+for num in range(0,20):
+    x = random.randrange(0,100)
+    x = sorted(reverse = True)
+    print(x)
+
+
+
+
