8906c8ab03c0ad1de44de1e7252487c6e88a7f81
of-curso-python <of-curso-python@gmail.com>
2017-06-16 08:25:35 -0600

    I-1

--- tarea.py
+++ tarea.py
@@ -1,10 +1,12 @@
-nombre=(input ("introduce tu nombre: "))
-año=int(input ("introduce año de nacimiento: "))
-resta=int(input ("introduce año que vas a sumar: "))
-añoa=int(input ("introduce año actual: "))
-edad22=añoa-año
-edadactual=edad22+resta
-edad = resta+año+edad22
 
 
-print(nombre," cumplira ",edadactual,"en el año ",edad)
+if __name__ == '__main__':
+    nombre=(input ("introduce tu nombre: "))
+    año=int(input ("introduce año de nacimiento: "))
+    resta=int(input ("introduce año que vas a sumar: "))
+    añoa=int(input ("introduce año actual: "))
+    edad22=añoa-año
+    edadactual=edad22+resta
+    edad = resta+año+edad22
+
+    print(nombre," cumplira ",edadactual,"en el año ",edad)
