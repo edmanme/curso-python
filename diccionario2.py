5f8bea1b4e9633a33870e6844d3faaa78ad0d783
Udem <udem@fedora.mshome.net>
2017-06-21 17:20:37 -0600

    diccionario 2 ejercicio

---
+++ diccionario2.py
@@ -0,0 +1,41 @@
+import random
+
+NOMBRES = [
+    'Ana',
+    'Pedro',
+    'Pablo',
+    'Ernesto',
+    'Ariel',
+    'Carlos',
+    'Luis',
+    'Oscar',
+    'Alicia',
+    'Maria',
+    'Brenda'
+]
+
+CIUDADES = [
+    'Managua',
+    'Masaya',
+    'Matagalpa',
+    'Chinandega',
+    'Somoto',
+    'Rivas'
+]
+
+
+def generar_diccionario_estudiantes():
+    estudiantes = {}
+
+    for nombre in NOMBRES:
+        estudiantes[nombre] = {
+            'edad': random.randrange(16, 30),
+            'anio': random.randrange(1, 5),
+            'cuidad': random.choice(CIUDADES)
+        }
+
+    return estudiantes
+dic={'nombre':generar_diccionario_estudiantes()}
+for i in dic:
+    print(dic[i])
+    
