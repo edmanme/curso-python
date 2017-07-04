bda9b2738dce108578451a07f2652c20d0d73d7a
Udem <udem@fedora.mshome.net>
2017-07-04 17:49:15 -0600

    clase 04/07/2017

--- python_web/proyecto_web/documentos/admin.py
+++ python_web/proyecto_web/documentos/admin.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from documentos.models import Documento
+
+class DocumentoAdmin(admin.ModelAdmin):
+	model = Documento
+# registrar modeladmin para cada modelo
+admin.site.register(Documento, DocumentoAdmin)
