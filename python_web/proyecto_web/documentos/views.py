9ba2229cb52cb1e1acb84c7e29d7bdc4f091ee62
Udem <udem@fedora.mshome.net>
2017-07-05 17:53:50 -0600

    documentos cambiados de html y documentos agregados

--- python_web/proyecto_web/documentos/views.py
+++ python_web/proyecto_web/documentos/views.py
@@ -4,3 +4,25 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+#las vistas manejan la logica del sitio web,
+ #importar clase base para vistas
+from django.views.generic import View
+from documentos.models import Documento
+
+#crear vista de Documentos
+#definir metodo get
+class Documentos(View):
+	def get(self, request, *args, **kwargs):
+
+		docs = Documento.objects.all()
+		#select * from documentos?_documento;
+
+		context = {
+
+		'docs':docs,
+		'encabezado':'Mis Documentos'
+		}
+
+
+		return render(request,'documentos.html',context)
--- python_web/proyecto_web/proyecto_web/settings.py
+++ python_web/proyecto_web/proyecto_web/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'proyecto_web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [templates],
+        'DIRS': ['template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
